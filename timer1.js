1569516f68754d6e6a5ab3ae65c00f10edc372e8
Rina Inada <indrn@icloud.com>
2023-06-10 22:36:20 +0000

    implemented timer1.js

---
+++ timer1.js
@@ -0,0 +1,13 @@
+const delays = process.argv.slice(2);
+
+if (delays.length === 0) {
+  console.log("Please enter valid number(s)");
+}
+
+for (const delay of delays) {
+  if (delay > 0 && isNaN(delay) !== true) {
+    setTimeout(() => {
+      process.stdout.write('\x07');
+    }, delay * 1000);
+  }
+}
